93e1e3c5ecf6eae96bce3554cdb6da4752b736bd
MolefeNthabane <nthabanemolefe@gmail.com>
2021-10-22 07:26:10 +0200

    Initial commit

---
+++ app/src/main/java/com/prototype/signinandsignup/loginActivity.java
@@ -0,0 +1,24 @@
+package com.prototype.signinandsignup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+
+public class loginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //for changing status bar icon color
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+        setContentView(R.layout.activity_login);
+    }
+    public void onLoginClick(View view){
+        startActivity(new Intent(this,RegisterActivity.class));
+        overridePendingTransition(R.anim.slide_in_right, R.anim.stay);
+    }
+}
